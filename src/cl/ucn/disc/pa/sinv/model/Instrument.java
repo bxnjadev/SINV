d44b171ab8445a66a496e58bd20acdac73918cbb
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-05-03 21:53:29 -0400

    feature: added method in instrument

--- src/cl/ucn/disc/pa/sinv/model/Instrument.java
+++ src/cl/ucn/disc/pa/sinv/model/Instrument.java
@@ -1,4 +1,28 @@
 package cl.ucn.disc.pa.sinv.model;
 
 public class Instrument {
+
+    private int stockAvailable;
+
+    public boolean hasStock() {
+        return stockAvailable > 0;
+    }
+
+    public void removeStock(int quantityStock) {
+        setStock(getStock() - quantityStock);
+    }
+
+    public int getStock() {
+        return stockAvailable;
+    }
+
+    private void setStock(int stockAvailable) {
+        this.stockAvailable = stockAvailable;
+
+        if (stockAvailable < 0) {
+            this.stockAvailable = 0;
+        }
+
+    }
+
 }
